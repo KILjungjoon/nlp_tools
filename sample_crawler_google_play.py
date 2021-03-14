6354d1710e506c6205a7045e4a8b85b9779a7335
hammerandnail <yelionel26@gmail.com>
2021-03-14 21:58:44 +0800

    Modified sample

--- sample_crawler_google_play.py
+++ sample_crawler_google_play.py
@@ -8,4 +8,4 @@ gp.url         = 'https://play.google.com/store/apps/details?id=air.com.speaking
 gp.result_file = './sample_crawler_result.csv'
 gp.scroll_cnt  = 10
 gp.headers     = {'accept-language': 'ko,ko-KR;q=0.9,en-US;q=0.8,en;q=0.7'}
-# gp.run_script()
+gp.run_script()
